8ce08ff9f80ce367f9b6040cf54356320069cb64
RaVi <77927691+RaViii1@users.noreply.github.com>
2022-10-17 11:07:47 +0200

    Update zad3_3.swift

--- lab3/zad3_3.swift
+++ lab3/zad3_3.swift
@@ -24,10 +24,16 @@ if let temp = readLine() {
 }
 str1 = name + ", " + name2 + ", " + lastName + ", " + year
 print(str1)
-//Polecenie 
+//Polecenie 2
 let nl : Int = Int(name.count) 
 let n12 : Int = name2.count 
 let srl : Int = lastName.count 
 let yl : Int = year.count 
 
-str1 = str1.remove(at: name) 
+var x = n12 + 2
+for _ in 1...x {
+    var i = str1.index(str1.startIndex, offsetBy: nl+1)
+    var removed_String = str1.remove(at: i)
+    }
+print(str1)
+//Polecenie 3
