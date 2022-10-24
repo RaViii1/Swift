0dfe27cf652ce2815ed06176d1d81ae9b018b4ae
RaVi <77927691+RaViii1@users.noreply.github.com>
2022-10-24 11:34:39 +0200

    Update zad4_2.swift

--- lab4/zad4_2.swift
+++ lab4/zad4_2.swift
@@ -1,1 +1,18 @@
+var val : Int = 0
+repeat{
+    print("Podaj liczbę całkowitą z zakresu 2-3010:")
+    if let temp = Int(readLine()!){
+        val = temp
+    }
+}while (val < 2 || val > 3010)
+
+if(val <= 100){
+   print("I wiek")  
+}
+else if (val % 100 == 0) {
+    print("\(val/100) Wiek")
+}
+else {
+    print("\(val / 100 + 1 ) wiek")
+}
 
