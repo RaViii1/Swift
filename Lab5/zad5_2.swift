eac245c80377352b189d97c0691cc1b6df303e72
RaVi <77927691+RaViii1@users.noreply.github.com>
2022-11-06 15:42:41 +0100

    Create zad5_2.swift

---
+++ Lab5/zad5_2.swift
@@ -0,0 +1,31 @@
+var num : Int = 0
+var i : Int = 2
+
+print("Podaj liczbe calkowitą: ")
+guard let num = Int(readLine()!) else{
+    fatalError("To nie jest liczba calkowita")
+}
+
+
+
+func checkingPrimeNumber(num: Int) -> Bool{
+  
+if(num == 1 || num == 0){
+    return false
+   }
+   
+for i in 2..<num{
+if (num % i == 0){
+        return false
+    }
+}
+
+return true
+}
+
+if(checkingPrimeNumber(num: num)){
+    print("\(num) to liczba pierwsza")
+}else{
+    print("\(num) nie jest liczbą pierwszą")
+}
+
