c13ea9ded9331212d404857e1ddeca20cd7223b2
RaVi <77927691+RaViii1@users.noreply.github.com>
2022-11-07 18:10:15 +0100

    Update zad5_4.swift

--- Lab5/zad5_4.swift
+++ Lab5/zad5_4.swift
@@ -14,3 +14,29 @@ if(reversed == str){
 else{
      print("string nie jest palindromem")
 }
+
+/////////////V2///////////////////////
+var tekst : String?
+print("Podaj tekst: ", terminator: "")
+tekst = String(readLine()!)
+
+if (tekst != nil) {
+    let tablica = Array(tekst!)
+    var palindrome = true
+
+    for i in 0...(tablica.count / 2){
+        if (tablica[i] != tablica[tablica.count - 1 - i]) {
+            palindrome = false
+            break
+        }
+    }
+    if (palindrome) {
+        print("Wyraz jest palindromem")
+    }
+    else {
+        print("Wyraz nie jest palindromem")
+    }
+}
+else {
+    print("Niepoprawne dane")
+}
