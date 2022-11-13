f079ca541e766f3b0d561e62ae85f4d017f0bea8
RaVi <77927691+RaViii1@users.noreply.github.com>
2022-11-13 18:36:51 +0100

    Update zad6_3.swift
    liczba do poprawy

--- Lab6/zad6_3.swift
+++ Lab6/zad6_3.swift
@@ -1,1 +1,44 @@
-zz
+import Swift
+import Foundation
+var arr : [Int] = []
+var rand : Int = 0
+var wczyt : Int = 0
+
+repeat{
+    print("Podaj wielkośc tablicy  (calkowita dodatnia): ")
+    guard let Liczba = Int(readLine()!) else{
+        fatalError("To nie jest liczba calkowita")
+    }
+}while(Liczba < 0)
+
+
+for _ in 1...Liczba{
+   if let temp = Int(readLine()!){
+    rand = temp
+    arr.append(rand)
+}
+}
+print("Zawartosc tablicy: ")
+for element in arr {
+  print(element)
+}
+
+print("Wczytana liczba: ")
+
+if let temp = Int(readLine()!){
+    wczyt = temp
+}
+
+if(wczyt == arr.first){
+    print("Wczytana liczba jest równa pierwszemu elementowi")
+}else{
+    print("Nie jest równa pierwszemu elementowi")
+}
+
+if(wczyt == arr.last){
+    print("Wczytana liczba jest równa ostatniemu elementowi")
+}else{
+    print("Nie jest równa ostatniemu elementowi")
+}
+
+
