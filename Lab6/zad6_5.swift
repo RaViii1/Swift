a4beb546a3b1461f51f3a6d61cfaa83cf03200e8
RaVi <77927691+RaViii1@users.noreply.github.com>
2022-11-14 11:19:37 +0100

    Update zad6_5.swift

--- Lab6/zad6_5.swift
+++ Lab6/zad6_5.swift
@@ -1,17 +1,23 @@
 var arr: [[Int]] = []
 var rand : Int = 0
 
-print("Podaj wielkośc tablicy  (a x b): ")
+var a : Int = 0
+var b : Int = 0
+repeat{
+    print("Podaj wielkośc tablicy  (a x b): ")
     guard let Liczba = Int(readLine()!) else{
         fatalError("To nie jest liczba calkowita")
     }
-guard let Liczba2 = Int(readLine()!) else{
+    a = Liczba
+    guard let Liczba2 = Int(readLine()!) else{
         fatalError("To nie jest liczba calkowita")
     }
+    b = Liczba2
+}while(a < 0 || b < 0)
     
 
-for i in 0...Liczba-1 {
-    for j in 0...Liczba2-1{
+for i in 0...a-1 {
+    for j in 0...b-1{
         rand = Int.random(in: 1...100)
         arr.append([rand])
     }
