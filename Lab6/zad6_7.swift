d54f5b0cb59e3b90bbd09bc833b79005ee5ae7b4
RaVi <77927691+RaViii1@users.noreply.github.com>
2022-11-19 18:58:54 +0100

    Update zad6_7.swift

--- Lab6/zad6_7.swift
+++ Lab6/zad6_7.swift
@@ -41,5 +41,57 @@ for _ in 1...arr.count-2 {
     if ((arr[index-1] < arr[index]) && (arr[index] > arr[index + 1])){
         Leng2 += 1
     }
+////////V2//////////
+    
+    var n: Int
+var indPocz = -1
+var liczbaPodciagu, maxLiczbaPodciagu: Int
+var t: [Int] = []
+
+repeat{
+    print("Podaj liczbe elementów tablicy, min. 3 elementowej")
+    guard let a = Int(readLine()!) else{
+        fatalError("To nie jest liczba calkowita!")
+    }
+    n = a
+}while n < 3
+
+//wczytanie
+for _ in 0..<n{
+    print("Podaj element calkowity")
+    guard let l = Int(readLine()!) else{
+        fatalError("To nie jest liczba calkowita")
+    }
+    t.append(l)
+}
+print("Wczytany ciąg:")
+print(t)
+
+//najdluzszy podciag rosnacy
+liczbaPodciagu = 1
+maxLiczbaPodciagu = 1
+for i in 0..<t.count-1{
+    if t[i] < t[i+1]{
+        liczbaPodciagu += 1
+    }else{
+        if liczbaPodciagu > maxLiczbaPodciagu{
+            maxLiczbaPodciagu = liczbaPodciagu
+            indPocz = i - maxLiczbaPodciagu + 1
+            liczbaPodciagu = 1
+        }
+    }
+}
+if liczbaPodciagu > maxLiczbaPodciagu{
+    maxLiczbaPodciagu = liczbaPodciagu
+    indPocz = t.count - maxLiczbaPodciagu
+}
+
+if maxLiczbaPodciagu == 1{
+    print("W ciagu nie ma podciagu rosnacego")
+}else{
+    print("Najdluzszy podciag rosnacy wystepuje od \(indPocz) i ma \(maxLiczbaPodciagu) elemen.:")
+    for i in indPocz..<indPocz+maxLiczbaPodciagu{
+        print(t[i])
+    }
 }
 
