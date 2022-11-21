fdfd98d0e36d66ec3e2e76fa0110cd34c40077fe
RaVi <77927691+RaViii1@users.noreply.github.com>
2022-11-21 11:33:26 +0100

    Update zad5_2.swift

--- Lab5/zad5_2.swift
+++ Lab5/zad5_2.swift
@@ -27,3 +27,26 @@ if(checkingPrimeNumber(num: num)){
     print("\(num) nie jest liczbą pierwszą")
 }
 
+////////////v2////////////////
+print("Podaj liczbę")
+guard let licz = Int(readLine()!) else {
+    fatalError("Nie jest to liczba")
+}
+
+if (licz < 2) {
+    print("Nie jest to liczba pierwsza")
+} else {
+    var i = 2
+    var pierwsza = true
+    while (i*i < licz) {
+        if (licz % i == 0) {
+            pierwsza = false
+        }
+        i = i + 1
+    }
+if (pierwsza) {
+        print("Pierwsza")
+    }else {
+        print("Nie pierwsza")
+    }
+}
