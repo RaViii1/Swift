32b114df76605ea04952ceddf3cfded6697bfc59
RaVi <77927691+RaViii1@users.noreply.github.com>
2022-11-21 15:19:37 +0100

    Update wszystko.swift

--- kol1/wszystko.swift
+++ kol1/wszystko.swift
@@ -74,3 +74,24 @@ for i in 0..<n{
 print("Najblizsza wartosc do poszukiwanej: ", arr[indx] )
 
 //Pierwsze
+import Foundation
+print("Wprowadz liczbe")
+guard let liczba = Int(readLine()!) else {
+    fatalError("To nie jest liczba calkowita")
+}
+if(liczba<2)
+{
+    print("To nie sa liczby pierwsze")
+}
+for i in 2..<liczba {
+    var pierwsza = true
+    for j in 2...Int(pow(Double(i), 0.5)+1) {
+        if (i % j == 0) {
+            pierwsza = false
+            break
+        }
+    }
+    if (pierwsza || i == 2) {
+        print(i)
+    }
+}
