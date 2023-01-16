8244a0becc3d21ba96f56c24b1bba8983a3957be
RaVi <77927691+RaViii1@users.noreply.github.com>
2023-01-16 18:59:46 +0100

    Create zad13_4.swift

---
+++ Lab12/zad13_4.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+enum BankomatErrors : Error{
+    case BrakSrodkow
+    case BrakWBankomacie
+    case DziennyLimit
+    case BrakNomina
+    case BlednaKwota
+}
+
+class Bankomat{
+    var banknoty : [n : Int, c: Int] = []
+    
+    init(bn) {
+        statements
+    }
+    func total () ->{
+        
+    }
+}
+
